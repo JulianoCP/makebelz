d5e63570f16a3b52df5ba32f35a1fdd97c025a60
Juliano Cesar Petini <juliano-petini@hotmail.com>
2019-09-27 22:15:18 -0300

    tela de login

--- MakeBelz/src/app/app-routing.module.ts
+++ MakeBelz/src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
+  { path: 'cad-manicure', loadChildren: './cad-manicure/cad-manicure.module#CadManicurePageModule' },
 ];
 
 @NgModule({
