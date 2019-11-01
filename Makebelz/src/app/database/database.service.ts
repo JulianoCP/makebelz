f1d2b49ab9df350b56f3beb82bdfd62a7121baac
LuizHenriqueBO <luiz_hickbarros@hotmail.com>
2019-11-01 16:20:36 -0300

    Alterações no diretório do docker

---
+++ Makebelz/src/app/database/database.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DatabaseService {
+  constructor(public firestore: AngularFirestore) {}
+
+  create(id, record, collection) {
+    return this.firestore
+      .collection(collection)
+      .doc(id)
+      .set(record);
+  }
+
+  // read()
+}
