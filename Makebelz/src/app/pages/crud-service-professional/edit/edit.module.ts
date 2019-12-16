c0c40ecc054f2d4fb5fbac53af8fbe1d83beb735
LuizHenriqueBO <luiz_hickbarros@hotmail.com>
2019-12-16 15:14:56 -0300

    Criando horário de serviços

--- Makebelz/src/app/pages/crud-service-professional/edit/edit.module.ts
+++ Makebelz/src/app/pages/crud-service-professional/edit/edit.module.ts
@@ -1,16 +1,23 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+import { Routes, RouterModule } from "@angular/router";
 
-import { IonicModule } from '@ionic/angular';
+import { IonicModule } from "@ionic/angular";
 
-import { EditPage } from './edit.page';
+import { EditPage } from "./edit.page";
 
 const routes: Routes = [
   {
-    path: '',
+    path: "",
     component: EditPage
+  },
+  {
+    path: "serviceProfessional",
+    loadChildren: () =>
+      import(
+        "../../crud-service-professional/crud-service-professional.module"
+      ).then(m => m.CrudServiceProfessionalPageModule)
   }
 ];
 
